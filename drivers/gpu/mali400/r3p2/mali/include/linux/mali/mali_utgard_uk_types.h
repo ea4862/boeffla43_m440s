bee2b588d8e4829587393fa52ef2a7367ec8e649
joohyoung Yang <yangjoo@debian.yangjoo.net>
2015-06-08 05:39:12 +0900

    add ramdisk and modify defconfig, mali version for M440S JB42

--- drivers/gpu/mali400/r3p2/mali/include/linux/mali/mali_utgard_uk_types.h
+++ drivers/gpu/mali400/r3p2/mali/include/linux/mali/mali_utgard_uk_types.h
@@ -729,7 +729,7 @@ typedef struct
  * Damn you, Samsung!
  */
 #undef _MALI_API_VERSION
-#define _MALI_API_VERSION 23
+#define _MALI_API_VERSION 17
 
 #define _MALI_UK_API_VERSION _MAKE_VERSION_ID(_MALI_API_VERSION)
 
